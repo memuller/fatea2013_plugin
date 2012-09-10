c3e869408c04c6f1f9a341e24b4e26fbdfebda58
Matheus E. Muller <hello@memuller.com>
2012-09-10 16:04:58 -0300

    Properly sorts courses.

--- presenters/Course.php
+++ presenters/Course.php
@@ -7,6 +7,17 @@
 		static $uses = array('styles', 'scripts', 'admin_scripts');
 		static $presents_for = array('custom_post' => 'course');
 		
+		static function build(){
+			parent::build();
+			$class = get_called_class(); $namespace = get_namespace($class); 
+
+			add_action('pre_get_posts', function($query) use($class, $namespace) {
+				if( !is_admin() && $query->is_main_query() && $query->query['post_type'] == 'course' ){
+					$query->set('order', 'ASC'); $query->set('orderby', 'title');
+				}
+			});
+		}
+
 		static function scripts(){
 			if(is_post_type_archive('course')){
 				wp_enqueue_script('jquery');
