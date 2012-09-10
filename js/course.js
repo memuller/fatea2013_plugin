5c7a6ad740096b6113d46ddbe8fc1d8fccdb39b2
Matheus E. Muller <hello@memuller.com>
2012-09-10 17:13:41 -0300

    Properly loads current course on bookmark/home linking.

--- js/course.js
+++ js/course.js
@@ -1,11 +1,24 @@
 jQuery(document).ready( function($) {
 	$('#scroll').carouFredSel({
 		direction: 'left',
+		start: true,
 		align: 'center',
 		prev: '#previous',
 		next: '#next',
 		auto: {play: false},
 		items: {start: true},
+		onCreate: function(items, sizes){
+			$('#name h2').html($(items).attr('title'));
+			
+			$previous = $(items).find('.navigation a.previous');
+			$next = $(items).find('.navigation a.next');
+
+			$('#previous p').html($previous.html());
+			$('#next p').html($next.html());
+
+			$('#previous').attr('href', $previous.attr('href'));
+			$('#next').attr('href', $next.attr('href'));
+		},
 		scroll: {
 			onAfter: function(oldItems, newItems, newSizes){
 				$('#name h2').html($(newItems).attr('title'));
