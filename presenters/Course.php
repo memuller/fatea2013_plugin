34eff0965490da879acd35f72de17463d46e3895
Matheus E. Muller <hello@memuller.com>
2012-09-09 22:30:45 -0300

    Fixes JS URL error (double backlashes).

--- presenters/Course.php
+++ presenters/Course.php
@@ -10,8 +10,8 @@
 		static function scripts(){
 			if(is_post_type_archive('course')){
 				wp_enqueue_script('jquery');
-				wp_enqueue_script('jquery-caroufredsel', static::url('/js/jquery-caroufredsel/jquery.carouFredSel-5.6.4-packed.js'), array('jquery') );
-				wp_enqueue_script('course', static::url('/js/course.js'));
+				wp_enqueue_script('jquery-caroufredsel', static::url('js/jquery-caroufredsel/jquery.carouFredSel-5.6.4-packed.js'), array('jquery') );
+				wp_enqueue_script('course', static::url('js/course.js'));
 			}
 
 		}
