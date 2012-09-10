d5effceb6da9780b7f2b5312bea7f5ad8c1729fc
Matheus E. Muller <hello@memuller.com>
2012-09-10 05:09:35 -0300

    Loads only pages on the thickbox; not all menu itens.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 jQuery(document).ready( function($) {
-	$('#menu a').colorbox({
+	$('#menu .menu-item-object-page a').colorbox({
 		inline: true, speed: 200,
 		href: function(){ 
 			return "#"+$.colorbox.element().attr('href').split('/').slice(-2)[0] ;
